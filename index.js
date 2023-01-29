d5839cac7d213dc194140f254611f32a1cc6178a
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-01-29 22:00:46 +0300

    Функция reverse()

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-// Функция addPrefix()
+// Функция reverse()
 
-const addPrefix = (arr, prefix) => {
+const names1 = ["john", "smith", "karl"];
+const names2 = [];
+const names3 = ["one", "two"];
+const names4 = ["john", "smith", "karl", "alan", "joe"];
+
+/*
+// Вариант реверса без изменения исходного массива
+const reverse = (arr) => {
   const result = [];
-  for (let i = 0; i < arr.length; i++) {
-    result[i] = `${prefix} ${arr[i]}`;
+  let j = 0;
+  for(let i = arr.length - 1; i >= 0; i--) {
+    result[j] = arr[i];
+    j++;
   }
-  return result;
+  arr = result;
+  return arr;
+};
+*/
+
+const reverse = (arr) => {
+  let j = 0;
+
+  for (let i = arr.length - 1; i >= arr.length / 2; i--) {
+    const first = arr[j];
+    arr[j] = arr[i];
+    arr[i] = first;
+    j++;
+  }
+  return arr;
 };
 
-const names = ["john", "smith", "karl"];
-
-console.log(addPrefix(names, "Mr"));
-
+console.log(reverse(names4));
