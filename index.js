883739f3c03d90bb12e42bc7f6ecf99767f3cbed
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-01-31 13:31:58 +0300

    Функция getTotalAmount()

--- index.js
+++ index.js
@@ -1,38 +1,20 @@
-const first = [];
-const second = [1, 2, 3];
-const third = [1, 2, 8];
-const fourth = [2, 2, 8];
-const fifth = [1, 2, -3];
-const sixth = [-3, 2, 1];
+const money0 = ["rub 50", "rub 60", "rub 70"];
+const money1 = ["eur 10", "usd 1", "usd 10", "rub 50", "usd 5"];
+const money2 = [ "eur 10", "usd 1", "eur 5", "rub 100", "eur 20", "eur 100", "rub 200"];
+const money3 = ["eur 10","rub 50", "eur 5", "rub 10", "rub 10", "eur 100", "rub 200"];
 
-// Функция getSameParity(). Вариант №1
-const getSameParity = (coll) => {
-  const result = [];
+// Функция getTotalAmount(), которая принимает на вход в виде массива кошелек с деньгами и название валюты и возвращает сумму денег указанной валюты. 
+const nameOfConst = (coll, currency) => {
+  let sum = 0;
 
   for (const item of coll) {
-    if (Math.abs(coll[0]) % 2 === 0) {
-      if (item % 2 === 0) {
-        result.push(item);
-      }
-    } else {
-      if (item % 2 !== 0) {
-        result.push(item);
-      }
+    let curr = item.slice(0, 3);
+    if (curr === currency) {
+      let value = Number(item.slice(4));
+      sum += value;
     }
   }
-  return result;
+  return sum;
 };
 
-// Функция getSameParity(). Вариант №2
-const getSameParitySecond = (coll) => {
-  const result = [];
-
-  for (const item of coll) {
-    if (Math.abs(coll[0]) % 2 === Math.abs(item % 2)) {
-      result.push(item);
-    }
-  }
-  return result;
-};
-
-console.log(getSameParitySecond(fourth));
+console.log(nameOfConst(money3 , "rub"));
