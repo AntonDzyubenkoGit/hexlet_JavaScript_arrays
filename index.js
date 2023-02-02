124147b4dee4cc8b3717947fe65f08328d58f1d6
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-02 20:00:04 +0300

    Функция countUniqChars()

--- index.js
+++ index.js
@@ -1,36 +1,56 @@
 var _ = require("lodash");
 
-// Функция getSameCount(), которая принимает на вход два массива и возвращающую количество общих уникальных значений в обоих массивах.
-const getSameCount = (collOne, collTwo) => {
-  const result = [];
-  const uniqOne = _.uniq(collOne);
-  const uniqTwo = _.uniq(collTwo);
+const friends1 = ["vasya", "kolya", "petya"];
+const friends2 = ["igor", "petya", "sergey", "vasya", "sasha"];
+const text1 = "yyab";
+const text2 = "You know nothing Jon Snow and don't tell me more your lies";
 
-  for (const uniqSymbol of uniqOne) {
-    if (uniqTwo.includes(uniqSymbol)) {
-      result.push(uniqSymbol);
+const test1 = _.intersection(friends1, friends2); // пересечение
+const test2 = _.union(friends1, friends2); // объединение
+const test3 = _.difference(friends1, friends2); // разность
+
+// Функция countUniqChars(), которая получает на вход строку и считает, сколько символов (без учёта повторяющихся символов) использовано в этой строке
+const countUniqChars = (string) => {
+  if (string.length === 0) {
+    return 0;
+  }
+
+  const charsArray = [];
+
+  for (let i = 0; i < string.length; i++) {
+    if (charsArray.includes(string[i]) === false) {
+      charsArray.push(string[i]);
     }
   }
+  return charsArray.length;
+};
+
+// Функция countUniqChars(). Вариант с _.uniq()
+const countUniqCharsByUniq = (string) => {
+  if (string.length === 0) {
+    return 0;
+  }
+  const charsArray = _.uniq(string);
+  return charsArray.length;
+};
+
+// Функция countUniqChars(). Вариант с _.union()
+const countUniqCharsByUnion = (str) => {
+  if (string.length === 0) {
+    return 0;
+  }
+  
+  const res = [];
+
+  for (const char of str) {
+    res.push(char);
+  }
+
+  const result = _.union(res, []);
 
   return result.length;
 };
 
-// Функция getSameCount(). Вариант с циклами
-
-const getSameCount_ = (collOne, collTwo) => {
-  let count = 0;
-  const uniqCollOne = _.uniq(collOne);
-  const uniqCollTwo = _.uniq(collTwo);
-
-  for (const itemOne of uniqCollOne) {
-    for (const itemTwo of uniqCollTwo) {
-      if (itemOne === itemTwo) {
-        count += 1;
-        break;
-      }
-    }
-  }
-  return count;
-};
-
-console.log(getSameCount_([1, 3, 2, 2, 3, 1, 2, 3, 2, 1, 3], [3, 1, 1, 2, 5])); // 1, 2, 3 = 3
+console.log(countUniqChars(text2));
+console.log(countUniqCharsByUniq(text2));
+console.log(countUniqCharsByUnion(text2));
