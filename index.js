71b6ff97eb099203cf76fe068b62ad0deb0633a3
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-02 13:36:18 +0300

    Функция makeCensored()

--- index.js
+++ index.js
@@ -1,47 +1,37 @@
-// Функция buildDefinitionList(), которая генерирует HTML список определений (теги <dl>, <dt> и <dd>) и возвращает получившуюся строку
-const definitions = [
-  ["Блямба", "Выпуклость, утолщения на поверхности чего-либо"],
-  ["Бобр", "Животное из отряда грызунов"],
-];
+const text = "hello the world and all the other planet in our galaxy";
+const sentence = "When you play the game of thrones, you win or you die";
 
-const buildDefinitionList = (coll) => {
-  if(coll.length === 0) {
-    return '';
-  }
-  const parts = [];
-  for (const item of coll) {
-    parts.push(`<dt>${item[0]}</dt><dd>${item[1]}</dd>`);
-  }
-  const innerValue = parts.join("");
-  const result = `<dl>${innerValue}</dl>`;
-  return result;
+// Функция capitalizeWords(), которая делает заглавной каждую первую букву слова в строке
+const capitalize = (string) => {
+  return `${string[0].toUpperCase()}${string.slice(1)}`;
 };
 
-console.log(buildDefinitionList([]));
+const capitalizeWords = (sentence) => {
+  const separator = " ";
+  const words = sentence.split(separator);
+  const capitalizeWords = [];
 
-
-
-
-
-// Функция buildHTMLList(), которая возвращает HTML-список (ul)
-const coll = ["milk", "butter", "bread", "onion", "carrot"];
-
-const buildHTMLListFirst = (coll) => {
-  let result = "<ul>";
-  for (const item of coll) {
-    result = `${result}<li>${item}</li>`;
+  for (const word of words) {
+    capitalizeWords.push(capitalize(word));
   }
-  result = `${result}</ul>`;
 
-  return result;
+  return capitalizeWords.join(separator);
 };
 
-const buildHTMLListSecond = (coll) => {
-  const parts = [];
-  for (const item of coll) {
-    parts.push(`<li>${item}</li>`);
+console.log(capitalizeWords(text));
+
+// Функция makeCensored(), которая заменяет каждое вхождение указанных слов в предложении на последовательность $#%! и возвращает полученную строку.
+
+const makeCensored = (string, stopWords) => {
+  const separator = " ";
+  const words = string.split(separator);
+  const result = [];
+
+  for (const word of words) {
+    stopWords.includes(word) ? result.push("$#%!") : result.push(word);
   }
-  const innerValue = parts.join("");
-  const result = `<ul>${innerValue}</ul>`;
-  return result;
+
+  return result.join(separator);
 };
+
+console.log(makeCensored(sentence, ["die", "play"]));
