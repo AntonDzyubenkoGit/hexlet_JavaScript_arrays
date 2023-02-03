cbfbc6c550706791f6ff63ecdef8b3b26d7f890b
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-03 20:28:54 +0300

    Функция getIntersectionOfSortedArrays()

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-// Функция isBracketStructureBalanced(),  которая принимает на вход строку, состоящую только из открывающих и закрывающих скобок разных типов, и проверяет, является ли эта строка сбалансированной.
-const testing1 = "(>"; // false
-const testing2 = "({})"; // true
-const testing3 = "[()]"; // true
-const testing4 = "({}[])"; // true
-const testing5 = "{<>}}"; // false
-const testing6 = "([)]"; // false
-
-const openingSymbols = ["(", "[", "{", "<"];
-const closingSymbols = [")", "]", "}", ">"];
-
-const isBracketStructureBalanced = (str) => {
-  if (str.length === 0) {
-    return true;
-  }
-  if (str.length % 2 !== 0) {
-    return false;
-  }
-
-  const stack = [];
-
-  for (const symbol of str) {
-    if (openingSymbols.includes(symbol)) {
-      const oppositeSymbol = closingSymbols[openingSymbols.indexOf(symbol)];
-      stack.push(oppositeSymbol);
-    } else if (symbol !== stack.pop()) {
-      return false;
-    }
-  }
-  return stack.length === 0;
+// Функция getIntersectionOfSortedArrays(), вариант с _.intersection()
+const _ = require("lodash");
+const getIntersectionOfSortedArraysByLodash = (arr1, arr2) => {
+  return _.intersection(arr1, arr2);
 };
 
-console.log(isBracketStructureBalanced(testing4));
+// Функция getIntersectionOfSortedArrays(), вариант с includes()
+const getIntersectionOfSortedArraysIncludes = (arr1, arr2) => {
+  const result = [];
+  for (let i = 0; i < arr2.length; i += 1) {
+    if (arr1.includes(arr2[i]) && !result.includes(arr2[i])) {
+      result.push(arr2[i]);
+    }
+  }
+  return result;
+};
+
+// Функция getIntersectionOfSortedArrays(), которая принимает на вход два отсортированных массива и находит их пересечение
+const getIntersectionOfSortedArrays = (arr1, arr2) => {
+  const result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] === arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+      j++;
+    } else {
+      arr1[i] > arr2[j] ? j++ : i++;
+    }
+  }
+
+  return [...new Set(result)];
+};
+
+
+
+console.log(
+  getIntersectionOfSortedArrays([10, 11, 24], [10, 13, 14, 18, 24, 30])
+);
+console.log(getIntersectionOfSortedArrays([10, 11, 24], [-2, 3, 4]));
+console.log(getIntersectionOfSortedArrays([], [2]));
+
+console.log(
+  getIntersectionOfSortedArrays([1, 1, 1, 2, 2, 2], [1, 1, 2, 2, 3, 3])
+);
