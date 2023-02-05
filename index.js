c1800f0c48f36f64c7c9b3ec6abf9a834846e7b1
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-05 18:34:51 +0300

    Функция flatten()

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-// Функция getMax(),  которая ищет в массиве максимальное значение и возвращает его
-const getMax = (coll) => {
-  if (coll.length === 0) {
-    return null;
-  }
+// Функция flatten(), которая принимает на вход массив и выпрямляет его: если элементами массива являются массивы, то flatten сводит всё к одному массиву, раскрывая один уровень вложенности
 
-  let [max, ...rest] = coll;
+const one = []; // []
+const two = [1, [3, 2], 9]; // [1, 3, 2, 9]
+const three = [1, [[2], [3]], [9]]; //[1, [2], [3], 9]
 
-  for (const integer of rest) {
-    if (integer > max) {
-      max = integer;
+const flatten = (coll) => {
+  let result = [];
+  for (const elem of coll) {
+    if (Array.isArray(elem)) {
+      result = [...result, ...elem];
+    } else {
+      result = [...result, elem];
     }
   }
-  return max;
+
+  return result;
 };
 
-console.log(getMax([]));
+const flattenTwo = (coll) => {
+  const result = [];
+  for (const elem of coll) {
+    if (Array.isArray(elem)) {
+      for (const elemArr of elem) {
+        result.push(elemArr);
+      }
+    } else {
+      result.push(elem);
+    }
+  }
+
+  return result;
+};
+
+console.log(flatten(two));
