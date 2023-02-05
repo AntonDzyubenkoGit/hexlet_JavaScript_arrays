9fe590ffdde029f9f05469e5d9d023726223ddd5
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-05 17:57:52 +0300

    Функция getMax()

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
-// Функция getTheNearestLocation(), которая находит ближайшее место к указанной точке на карте и возвращает его.
-const locations = [
-  ["Park", [10, 5]],
-  ["Sea", [1, 3]],
-  ["Museum", [8, 4]],
-];
-
-const currentPoint = [5, 5];
-
-const getDistance = ([x1, y1], [x2, y2]) => {
-  const xs = x2 - x1;
-  const ys = y2 - y1;
-
-  return Math.sqrt(xs ** 2 + ys ** 2);
-};
-
-const getTheNearestLocation = (locations, currentPoint) => {
-  if (locations.length === 0) {
+// Функция getMax(),  которая ищет в массиве максимальное значение и возвращает его
+const getMax = (coll) => {
+  if (coll.length === 0) {
     return null;
   }
 
-  let [nearestLocation] = locations;
-  const [, nearestPoint] = nearestLocation;
-  let lowestDistance = getDistance(currentPoint, nearestPoint);
+  let [max, ...rest] = coll;
 
-  for (const location of locations) {
-    const [, point] = location;
-    const distance = getDistance(currentPoint, point);
-    if (distance < lowestDistance) {
-      lowestDistance = distance;
-      nearestLocation = location;
+  for (const integer of rest) {
+    if (integer > max) {
+      max = integer;
     }
   }
-  return nearestLocation;
+  return max;
 };
 
-console.log(getTheNearestLocation(locations, currentPoint));
+console.log(getMax([]));
