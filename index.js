e316197afb31a2e94d2444526ca012ec6a84469d
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-06 12:31:02 +0300

    Испытания. Javascript: Сравнение версий

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-// Испытания. Javascript: Треугольник Паскаля. Решение с определением элемента по формуле
-const pascal = (row) => {
-  const line = [1];
+// Испытания. Javascript: Сравнение версий
+const compareVersion = (a, b) => {
+  const arrA = a.split(".");
+  const arrB = b.split(".");
+  let diff = 0;
+  const lenghtArr = arrA.length;
 
-  for (let i = 0; i < row; i += 1) {
-    const element = line[i] * ((row - i) / (i + 1));
-    line.push(element);
+  for (let i = 0; i < lenghtArr; i++) {
+    if (arrA[i] === arrB[i]) {
+      diff = 0;
+    } else if (Number(arrA[i]) > Number(arrB[i])) {
+      diff = 1;
+      break;
+    } else {
+      diff = -1;
+    }
   }
-
-  return line;
+  return diff;
 };
 
-console.log(pascal(5));
+// Испытания. Javascript: Сравнение версий. Версия без цикла, но только для одного числа после точки
+const compareVersion2 = (first, second) => {
+  const [a1, b1] = first.split(".");
+  const [a2, b2] = second.split(".");
+
+  const major = Math.sign(a1 - a2);
+  const minor = Math.sign(b1 - b2);
+
+  return major === 0 ? minor : major;
+};
+
+console.log(compareVersion("3.2", "2.12"));
